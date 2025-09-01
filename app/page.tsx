a418a484d9e81ed04903a27aa33a05a837b6bf00
Shawn Donoho <shawn@locuna.com>
2025-09-01 15:56:45 -0500

    add css-based scroll engine

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,43 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect, useRef, useCallback } from "react";
 import styles from "./page.module.scss";
 
 export default function Home() {
+  const [articles, setArticles] = useState([1, 2]);
+  const observer = useRef<IntersectionObserver>();
+  const lastArticleRef = useCallback(
+    (node) => {
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setArticles((prevArticles) => [...prevArticles, prevArticles.length + 1]);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    []
+  );
+
   return (
     <section className={styles.container}>
-      <article></article>
-      <article></article>
+      {articles.map((articleNumber, index) => {
+        if (index + 1 === articles.length) {
+          return (
+            <article
+              ref={lastArticleRef}
+              key={articleNumber}
+            >
+              <h1>{articleNumber}</h1>
+            </article>
+          );
+        }
+        return (
+          <article key={articleNumber}>
+            <h1>{articleNumber}</h1>
+          </article>
+        );
+      })}
     </section>
   );
 }
